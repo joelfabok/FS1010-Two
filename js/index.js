f49db9794dd969bae085debcf60fe61e937bf035
Joel Fabok <joelfabok@gmail.com>
2021-02-03 22:17:56 -0800

    Made it so when i add an iteam it clears the input. Also made it so you have to add something to the input field other wise you get an error.

--- js/index.js
+++ js/index.js
@@ -3,23 +3,27 @@ $(document).ready(
         $('.add').click(
             function(){
                 let toAdd = $('input[name=item]').val();
-                 $('ol').append('<li>' + toAdd + '</li>' + '<button class="close">X</button>');
-                 
-                 if( !$(this).val() ) {
-                    $(this).parents('').addClass('warning');
-              }
+
+                if (!$('input').val()) {
+                    alert('Please Add Item');
+                  }
+                  
+                  else {
+                    $('ul').append('<li>' + toAdd + '</li>' + '<button class="close">X</button>');
+                  }
                     
             });
             
         $(document).on('dblclick','li', function(){
-        $(this).toggleClass('checked').toggleClass('strike').fadeout('slow');
+        $(this).toggleClass('checked').toggleClass('strike');
             
         });
-        $('input').click(function() {
-            $(this).val('');
-          });   
+
+        $('.add').click(function(){				 
+                $('input[name=item]').val('');
+        });
           
-          $('ol').sortable(); 
+          $('ul').sortable(); 
       
     }
     
