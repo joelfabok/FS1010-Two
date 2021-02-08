44f3261f2d9e35610355987f66f53cd30f79585c
Joel Fabok <joelfabok@gmail.com>
2021-02-08 09:34:02 -0800

    Made it so you can click enter to add list item

--- js/index.js
+++ js/index.js
@@ -1,21 +1,45 @@
 $(document).ready(
     function(){
-        let key = '13';
+
+        // ***********************************//
+        // This Will Add Item To List On Enter//
+        // ***********************************//
+        
+        $('input').keydown(function(e) {
+            if (e.which === 13) {
+                if (!$('input').val()) {                
+                    alert('Please Add Item');           
+                  }                                     
+                  
+                  else {
+                    $("ul").prepend("<li>" + this.value + '<button class="close">X</button>' + "</li>");
+                  }
+                this.value = "";
+            }
+        });
+
+        // **********************************************//
+        // This Will Add Item To List When Add Is Clicked//
+        // **********************************************//
         $('.add').click(
             function(){
                 let toAdd = $('input[name=item]').val();
-
                 if (!$('input').val()) {
                     alert('Please Add Item');
                   }
                   
                   else {
-                    $('ul').append('<li id="what">' + toAdd + '<button class="close">X</button>' + '</li>');
+                    $('ul').prepend('<li>' + toAdd + '<button class="close">X</button>' + '</li>');
                   }
+                toAdd = "";
+        
             });
             
         $(document).on('click','li', function(){
+            let last = $(".list li").last();
+            let first = $(".list li").first();
         $(this).toggleClass('checked').toggleClass('strike');
+
         });
 
         $('.add').click(function(){				 
@@ -28,8 +52,7 @@ $(document).ready(
         });
 
         
-
-        
-        
     }
 );
+
+
