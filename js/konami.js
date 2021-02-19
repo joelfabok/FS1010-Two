03d171f617320d82272c776b4ecf0cf9a8cd617b
Joel Fabok <joelfabok@gmail.com>
2021-02-18 21:56:42 -0800

    Changed my konami code. and finished up.

--- js/konami.js
+++ js/konami.js
@@ -1,35 +1,52 @@
 
-let Konami = Konami || {};
 
-Konami.key = '38384040373937396665';
-
-Konami.onCode = function(callback) {
-    let input = '';
-    $(document).on("keydown", function(e) {
-        input += ("" + e.keyCode);
-        if (input === Konami.key) {
-            if(typeof callback == 'undefined') {
-                $('main').css({
-                    'display': 'none',
-                }); 
-                
-                $('body').css ({
-                    'background-image' : "url('https://media4.giphy.com/media/eiGU1eOEoxIIYL4I42/giphy.gif')",
-                    'background-repeat' : 'no-repeat',
-                    'background-position' : 'center',  
-                })
-            }
-            else {
-                return callback();
-            }
+let global = {
+  
+  konami: function() {
+    let konamikeys = [38,38,40,40,37,39,37,39,66,65], 
+        started = false, 
+        count = 0;
+    
+    $(document).keydown(function(e){
+      let reset = function() {
+        started = false; 
+        count = 0;
+        return;
+      };
+      
+      key = e.keyCode;
+      
+      if(!started){
+        if(key == 38){
+          started = true;
         }
-        if (!Konami.key.indexOf(input)) return;
-        input = ("" + e.keyCode);
+      }
+      
+      if (started){
+        
+        if (konamikeys[count] == key){
+          count++;
+        } else {
+          reset();
+        }
+        if (count == 10){
+          $('main').css({
+            'display': 'none',
+        }); 
+        
+        $('body').css ({
+            'background-image' : "url('https://media4.giphy.com/media/eiGU1eOEoxIIYL4I42/giphy.gif')",
+            'background-repeat' : 'no-repeat',
+            
+        })
+        setTimeout(function(){
+          location.reload();
+         },5000);
+          
+        }
+      } 
     });
+  }
 }
 
-Konami.offCode = function() {
-    $(document).off("keydown");
-}
-
-Konami.onCode();
+global.konami();
